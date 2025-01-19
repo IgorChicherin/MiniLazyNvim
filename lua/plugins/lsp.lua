e49f882658d23a364b4a0b2835c828b686735902
Igor Chicherin <ashzak990@gmail.com>
2025-01-19 17:14:28 +0300

    fix file nammg

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,34 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+			dependencies = { "stevearc/dressing.nvim", opts = {}, event = "VeryLazy" },
+			opts = {
+				ensure_installed = {
+					"stylua",
+					"shellcheck",
+					"shfmt",
+					"flake8",
+				},
 			},
+			config = function()
+				-- import mason
+				local mason = require("mason")
+
+				-- enable mason and configure icons
+				mason.setup({
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				})
+			end,
 		},
+		{ "williamboman/mason-lspconfig.nvim", config = function() end },
 	},
 }
