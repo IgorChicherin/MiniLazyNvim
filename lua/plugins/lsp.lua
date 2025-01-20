51bde76166f246c085722559f21bcc0455cf206c
Igor Chicherin <ashzak990@gmail.com>
2025-01-20 17:54:01 +0300

    cmp keys cahnged

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -154,15 +154,38 @@ return {
 					end
 
 					local cmp = require("cmp")
-					map(
-						"<CR>",
-						cmp.mapping.confirm({
-							behavior = cmp.ConfirmBehavior.Replace,
-							select = true,
+					cmp.setup({
+						mapping = cmp.mapping.preset.insert({
+							["<A-j>"] = cmp.mapping.select_prev_item(),
+							["<A-k>"] = cmp.mapping.select_next_item(),
+							["<A-l>"] = cmp.mapping.confirm({ select = true }),
+
+							["<C-e>"] = cmp.mapping({
+								i = cmp.mapping.abort(),
+								c = cmp.mapping.close(),
+							}),
+
+							["<C-d>"] = cmp.mapping.scroll_docs(-4),
+							["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+							["<A-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+							["<CR>"] = cmp.mapping.confirm({
+								select = false,
+							}),
+							["<Tab>"] = cmp.mapping(function(fallback)
+								if cmp.visible() then
+									cmp.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true })
+								elseif require("luasnip").expand_or_jumpable() then
+									vim.fn.feedkeys(
+										vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+										""
+									)
+								else
+									fallback()
+								end
+							end, { "i", "s" }),
 						}),
-						"Apply",
-						{ "i" }
-					)
+					})
 				end,
 			})
 
