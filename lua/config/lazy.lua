03c90f6dbfc9ab1c837e44f64d5349c05ab9dc55
Igor Chicherin <ashzak990@gmail.com>
2025-01-22 12:13:28 +0300

    supress annoying messages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
   install = { colorscheme = { "tokyonight", "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true, notify = false },
+  change_detection = { notify = false },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
