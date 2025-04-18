235df56ba99f52ad39de9159df79360f7f175a8f
Igor <ashzak990@gmail.com>
2025-04-18 14:00:45 +0300

    keymaps update

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,30 @@
 
 local map = vim.keymap.set
 
+-- local keycode = vim.keycode or function(x)
+--   return vim.api.nvim_replace_termcodes(x, true, true, true)
+-- end
+--
+-- local keys = {
+--   ["cr"] = keycode("<CR>"),
+--   ["ctrl-y"] = keycode("<C-y>"),
+--   ["ctrl-y_cr"] = keycode("<C-y><CR>"),
+-- }
+--
+-- _G.cr_action = function()
+--   if vim.fn.pumvisible() ~= 0 then
+--     -- If popup is visible, confirm selected item or add new line otherwise
+--     local item_selected = vim.fn.complete_info()["selected"] ~= 1
+--     return item_selected and keys["ctrl-y"] or keys["ctrl-y_cr"]
+--   else
+--     -- If popup is not visible, use plain `<CR>`. You might want to customize
+--     -- according to other plugins. For example, to use 'mini.pairs', replace
+--     -- next line with `return require('mini.pairs').cr()`
+--     return keys["cr"]
+--   end
+-- end
+--
+-- map("i", "<CR>", "v:lua._G.cr_action()", { expr = true })
 map("n", "<Esc>", function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
     if vim.api.nvim_win_get_config(win).relative == "win" then
@@ -13,7 +37,6 @@ map("n", "<Esc>", function()
 end)
 
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
-map("i", "jk", "<Esc>")
 
 -- Diagnostic keymaps
 map("n", "<leader>x", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
