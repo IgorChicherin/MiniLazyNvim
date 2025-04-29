8e518266f18d61d79a78f288d4a30b941003e9a3
ashzak <ashzak990@gmail.com>
2025-04-29 15:58:15 +0300

    mini keymaps removed

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -12,35 +12,6 @@ map("n", "<Esc>", function()
   end
 end)
 
-local keycode = vim.keycode or function(x)
-  return vim.api.nvim_replace_termcodes(x, true, true, true)
-end
-
-local keys = {
-  ["cr"] = keycode("<CR>"),
-  ["ctrl-y"] = keycode("<C-y>"),
-  ["ctrl-n-y"] = keycode("<C-n><C-y>"),
-  ["ctrl-y_cr"] = keycode("<C-y><CR>"),
-}
-
-_G.cr_action = function()
-  if vim.fn.pumvisible() ~= 0 then
-    -- If popup is visible, confirm selected item or add new line otherwise
-    local item_selected = vim.fn.complete_info()["selected"] ~= -1
-    if item_selected then
-      return item_selected and keys["ctrl-y"] or keys["ctrl-y_cr"]
-    end
-    return keys["ctrl-n-y"]
-  else
-    -- If popup is not visible, use plain `<CR>`. You might want to customize
-    -- according to other plugins. For example, to use 'mini.pairs', replace
-    -- next line with `return require('mini.pairs').cr()`
-    return keys["cr"]
-  end
-end
-
-map("i", "<CR>", "v:lua._G.cr_action()", { expr = true })
-
 map("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
