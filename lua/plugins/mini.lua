872a6fc7b7cf3d04cc1a9c1f7b8090b80ada0a4b
ashzak <ashzak990@gmail.com>
2025-05-01 10:24:31 +0300

    hipatterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -9,6 +9,19 @@ return {
       require("mini.move").setup()
       require("mini.pairs").setup()
       require("mini.splitjoin").setup()
+      local hipatterns = require("mini.hipatterns")
+      hipatterns.setup({
+        highlighters = {
+          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+          -- Highlight hex color strings (`#rrggbb`) using that color
+          hex_color = hipatterns.gen_highlighter.hex_color(),
+        },
+      })
       require("mini.surround").setup({
         -- Module mappings. Use `''` (empty string) to disable one.
         mappings = {
