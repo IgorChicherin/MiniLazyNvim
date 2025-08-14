84a71aa6acb3b14b4610d2e58e58564bdbf9e318
Igor Chicherin <ashzak990@gmail.com>
2025-08-14 15:47:12 +0300

    multiselect gitignore

--- lua/plugins/gitignore.lua
+++ lua/plugins/gitignore.lua
@@ -1,6 +1,7 @@
 return {
-	"wintermute-cell/gitignore.nvim",
-	config = function()
-		require("gitignore")
-	end,
+  "wintermute-cell/gitignore.nvim",
+  config = function()
+    require("gitignore")
+  end,
+  dependencies = { "nvim-telescope/telescope.nvim" },
 }
