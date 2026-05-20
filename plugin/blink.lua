7587d9873c88c441b9c4adb20f5eeb327b12f1ff
Igor Chicherin <ashzak990@gmail.com>
2026-05-20 13:16:58 +0300

    lazydev

--- plugin/blink.lua
+++ plugin/blink.lua
@@ -9,13 +9,6 @@ require("blink.cmp").setup({
   appearance = { nerd_font_variant = "mono" },
   completion = { documentation = { auto_show = false } },
   signature = { enabled = true },
-  sources = { default = { "lsp", "path", "snippets", "buffer" } },
+  sources = { default = { "lsp", "path", "snippets", "buffer", "lazydev" } },
   fuzzy = { implementation = "prefer_rust_with_warning" },
-  providers = {
-    lazydev = {
-      name = "LazyDev",
-      module = "lazydev.integrations.blink",
-      score_offset = 100,
-    },
-  },
 })
