4cf7c08a58a70d7375b7bebb69e434103de0d36b
Igor Chicherin <ashzak990@gmail.com>
2026-06-17 16:54:50 +0300

    dap interface fixes

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -16,7 +16,7 @@ vim.schedule(function()
 
   -- Now load dap-view and dependencies
   vim.pack.add({
-    "https://github.com/igorlfs/nvim-dap-view",
+    { src = "https://github.com/igorlfs/nvim-dap-view", version = vim.version.range("1.*") },
     "https://github.com/nvim-neotest/nvim-nio",
     "https://github.com/mfussenegger/nvim-dap-python",
     "https://github.com/leoluz/nvim-dap-go",
@@ -24,21 +24,22 @@ vim.schedule(function()
     "https://github.com/williamboman/mason.nvim",
   })
 
+  require("mason-nvim-dap").setup({
+    ensure_installed = { "codelldb" },
+    automatic_setup = true,
+  })
   -- nvim-dap-view config
   require("dap-view").setup({
     winbar = {
+      sections = { "console", "watches", "scopes", "exceptions", "breakpoints", "threads", "repl" },
       show = true,
-      default_section = "scopes",
+      default_section = "console",
       show_keymap_hints = true,
-      controls = {
-        enabled = true,
-        position = "left",
-        buttons = { "play", "step_over", "step_into", "step_out", "run_last", "terminate" },
-      },
     },
     windows = {
       size = 15,
       position = "below",
+      terminal = { hide = true },
     },
     virtual_text = {
       enabled = true,
@@ -90,7 +91,9 @@ vim.schedule(function()
         type = "codelldb",
         request = "attach",
         name = "Attach to process",
-        pid = function() return require("dap.utils").pick_process() end,
+        pid = function()
+          return require("dap.utils").pick_process()
+        end,
         cwd = "${workspaceFolder}",
         port = 13000,
       },
@@ -100,14 +103,30 @@ end)
 
 -- DAP keymaps
 local map = vim.keymap.set
-map("n", "<F5>", function() require("dap").continue() end, { desc = "Run/Continue" })
-map("n", "<F7>", function() require("dap").step_into() end, { desc = "Step Into" })
-map("n", "<F4>", function() require("dap").run_last() end, { desc = "Run Last" })
-map("n", "<F9>", function() require("dap").step_out() end, { desc = "Step Out" })
-map("n", "<F8>", function() require("dap").step_over() end, { desc = "Step Over" })
-map("n", "<F10>", function() require("dap").terminate() end, { desc = "Terminate" })
-map("n", "<leader>du", function() require("dap-view").toggle() end, { desc = "Toggle DAP View" })
-map("n", "<leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+map("n", "<F5>", function()
+  require("dap").continue()
+end, { desc = "Run/Continue" })
+map("n", "<F7>", function()
+  require("dap").step_into()
+end, { desc = "Step Into" })
+map("n", "<F4>", function()
+  require("dap").run_last()
+end, { desc = "Run Last" })
+map("n", "<F9>", function()
+  require("dap").step_out()
+end, { desc = "Step Out" })
+map("n", "<F8>", function()
+  require("dap").step_over()
+end, { desc = "Step Over" })
+map("n", "<F10>", function()
+  require("dap").terminate()
+end, { desc = "Terminate" })
+map("n", "<leader>du", function()
+  require("dap-view").toggle()
+end, { desc = "Toggle DAP View" })
+map("n", "<leader>db", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Toggle Breakpoint" })
 map("n", "<leader>dB", function()
   local condition = vim.fn.input("Breakpoint condition (optional): ")
   local hit_condition = vim.fn.input("Hit count (optional): ")
